e8437d45d059997b6369ba11667ef217c3ce79da
Alan Gardner <alan@armoin.com>
2014-06-30 00:16:10 +0100

    Debugging Heroku

--- app.rb
+++ app.rb
@@ -9,6 +9,9 @@ configure do
   $COUCH = CouchRest.new ENV["COUCHDB_URL"]
   $COUCH.default_database = ENV["COUCHDB_DEFAULT_DB"]
   $COUCHDB = $COUCH.default_database
+  p env: ENV
+  p couch: $COUCH
+  p couchdb: $COUCHDB
 end
 
 require File.expand_path('models/asset', File.dirname(__FILE__))
