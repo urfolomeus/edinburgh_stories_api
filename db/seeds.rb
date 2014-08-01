4c1c0f0c413246a68f90e5b31e5cabdc33a486f9
Alan Gardner <alan@armoin.com>
2014-08-01 07:58:28 +0100

    Importing data from couch to postgres
    Signed-off-by: Alan Gardner <alan@armoin.com>

---
+++ db/seeds.rb
@@ -0,0 +1,290 @@
+Asset.create(
+  title: "Orientation test 2",
+  file_type: "image",
+  url: "https://edinburgh-stories-staging.s3.amazonaws.com/uploads/asset/source/f564ea92-dc2b-40e6-bf51-b74ed8cc465d/image.jpg",
+  description: "",
+  year: "2014",
+  month: "7",
+  day: "14",
+  is_readable: false,
+  updated_at: "2014-07-18T22:54:41.128Z",
+  created_at: "2014-07-14T11:43:23.557Z"
+)
+Asset.create(
+  title: "Barking",
+  file_type: "image",
+  url: "https://edinburgh-stories-staging.s3.amazonaws.com/uploads/asset/source/95a7ac0e-6005-4c63-8597-df876ae5e40e/image.jpg",
+  description: "Always with the barking",
+  year: "2014",
+  month: "7",
+  day: "14",
+  is_readable: false,
+  updated_at: "2014-07-18T22:54:35.805Z",
+  created_at: "2014-07-14T11:42:25.248Z",
+)
+Asset.create(
+  title: "New library",
+  file_type: "image",
+  url: "https://edinburgh-stories-staging.s3.amazonaws.com/uploads/asset/source/13e331da-ae95-4cf2-80b0-ec2169e4f423/image.jpg",
+  description: "Code the city.",
+  year: "2014",
+  month: "6",
+  day: "30",
+  is_readable: false,
+  updated_at: "2014-07-18T22:54:39.406Z",
+  created_at: "2014-07-14T11:38:44.801Z",
+)
+Asset.create(
+  title: "St Giles",
+  file_type: "image",
+  url: "https://edinburgh-stories-staging.s3.amazonaws.com/uploads/asset/source/0cf083e26f4edb8fa43b91157d1f578f/original.jpg",
+  description: "dky;dkif;dkhg;khjd;kh",
+  year: "2014",
+  month: "7",
+  day: "9",
+  is_readable: false,
+  updated_at: "2014-07-18T22:54:43.293Z",
+  created_at: "2014-07-09T08:40:04.336Z",
+)
+Asset.create(
+  title: "Princes Street",
+  file_type: "image",
+  url: "https://edinburgh-stories-staging.s3.amazonaws.com/uploads/asset/source/23a5c56f1873115f6c2b9dd8ae1ea7ca/original.JPG",
+  description: "Princes Street",
+  year: "2006",
+  month: "11",
+  day: "10",
+  is_readable: false,
+  updated_at: "2014-07-18T22:54:41.796Z",
+  created_at: "2014-07-11T11:33:56.569Z",
+)
+Asset.create(
+  title: "Renaissance Garden",
+  file_type: "image",
+  url: "https://edinburgh-stories-staging.s3.amazonaws.com/uploads/asset/source/23a5c56f1873115f6c2b9dd8ae1eb1b4/original.jpg",
+  description: "Formal garden in italy",
+  year: "2014",
+  month: "6",
+  day: "11",
+  is_readable: false,
+  updated_at: "2014-07-18T22:54:42.603Z",
+  created_at: "2014-07-11T11:26:08.945Z",
+)
+Asset.create(
+  title: "The Shore Leith",
+  file_type: "image",
+  url: "https://edinburgh-stories-staging.s3.amazonaws.com/uploads/asset/source/23a5c56f1873115f6c2b9dd8ae1ebc72/original.JPG",
+  description: "View of Leith's port",
+  year: "2014",
+  month: "7",
+  day: "11",
+  is_readable: false,
+  updated_at: "2014-07-18T22:54:44.072Z",
+  created_at: "2014-07-11T11:22:49.078Z",
+)
+Asset.create(
+  title: "Dugald Stewart Monument on Calton Hill",
+  file_type: "image",
+  url: "https://edinburgh-stories-staging.s3.amazonaws.com/uploads/asset/source/23a5c56f1873115f6c2b9dd8ae1ec67f/original.JPG",
+  description: "Classic view of Edinburgh from Calton Hill",
+  year: "2014",
+  month: "7",
+  day: "11",
+  is_readable: false,
+  updated_at: "2014-07-18T22:54:37.303Z",
+  created_at: "2014-07-11T11:20:00.049Z",
+)
+Asset.create(
+  title: "Construction at Fountainbridge",
+  file_type: "image",
+  url: "https://edinburgh-stories-staging.s3.amazonaws.com/uploads/asset/source/23a5c56f1873115f6c2b9dd8ae1ed453/original.jpg",
+  description: "Digger at Fountainbridge",
+  year: "2014",
+  month: "1",
+  day: "11",
+  is_readable: false,
+  updated_at: "2014-07-18T22:54:36.600Z",
+  created_at: "2014-07-11T11:19:21.129Z",
+)
+Asset.create(
+  title: "In the marsh bunker 2",
+  file_type: "image",
+  url: "https://edinburgh-stories-staging.s3.amazonaws.com/uploads/asset/source/23a5c56f1873115f6c2b9dd8ae1ed9d0/original.jpg",
+  description: "Second attempt",
+  year: "1908",
+  month: "8",
+  day: "10",
+  is_readable: false,
+  updated_at: "2014-07-18T22:54:38.728Z",
+  created_at: "2014-07-11T11:14:19.960Z",
+)
+Asset.create(
+  title: "In the marsh bunker",
+  file_type: "image",
+  url: "https://edinburgh-stories-staging.s3.amazonaws.com/uploads/asset/source/23a5c56f1873115f6c2b9dd8ae1ee3af/original.jpg",
+  description: "Mortonhall golf club",
+  year: "1908",
+  month: "8",
+  day: "11",
+  is_readable: false,
+  updated_at: "2014-07-18T22:54:37.948Z",
+  created_at: "2014-07-11T11:12:04.836Z",
+)
+Asset.create(
+  title: "Track cyclists, Meadowbank Velodrome",
+  file_type: "image",
+  url: "https://edinburgh-stories-staging.s3.amazonaws.com/uploads/asset/source/23a5c56f1873115f6c2b9dd8ae1eed3f/original.png",
+  description: "Watching cyclists training session at Velodrome",
+  year: "2013",
+  month: "9",
+  day: "3",
+  is_readable: false,
+  updated_at: "2014-07-18T22:54:45.867Z",
+  created_at: "2014-07-11T11:03:04.291Z",
+)
+Asset.create(
+  title: "Track cyclist, Meadowbank Velodrome",
+  file_type: "image",
+  url: "https://edinburgh-stories-staging.s3.amazonaws.com/uploads/asset/source/23a5c56f1873115f6c2b9dd8ae1efc1b/original.png",
+  description: "Taking pictures at Velodrome for library archive.",
+  year: "2013",
+  month: "9",
+  day: "3",
+  is_readable: false,
+  updated_at: "2014-07-18T22:54:44.998Z",
+  created_at: "2014-07-11T10:47:30.870Z",
+)
+Asset.create(
+  title: "North Berwick",
+  file_type: "image",
+  url: "https://edinburgh-stories-staging.s3.amazonaws.com/uploads/asset/source/777c5ef8-7b24-4c18-a112-3b290ce386e4/20140412_125629.jpg",
+  description: "A sunny blustery day in North Berwick.",
+  year: "2014",
+  month: "4",
+  day: "12",
+  is_readable: false,
+  updated_at: "2014-07-18T22:54:40.231Z",
+  created_at: "2014-07-18T09:21:29.652Z",
+)
+Asset.create(
+  title: "Hermitage of Braid",
+  file_type: "image",
+  url: "https://edinburgh-stories-staging.s3.amazonaws.com/uploads/asset/source/2d2cc643faa8c954e7e24139111fb991/original.JPG",
+  description: "Summer flowers trees countryside",
+  is_readable: false,
+  updated_at: "2014-07-08T08:43:42.758Z",
+  created_at: "2014-07-08T08:43:36.383Z",
+  year: 2014
+)
+Asset.create(
+  title: "Usher Hall Community Choir",
+  file_type: "image",
+  url: "https://edinburgh-stories-staging.s3.amazonaws.com/uploads/asset/source/2d2cc643faa8c954e7e24139111fc72f/original.jpg",
+  description: "Sitting in the audience and singing along",
+  is_readable: false,
+  updated_at: "2014-07-08T08:23:10.092Z",
+  created_at: "2014-07-08T08:23:03.419Z",
+  year: 2014
+)
+Asset.create(
+  title: "A field",
+  file_type: "image",
+  url: "https://edinburgh-stories-staging.s3.amazonaws.com/uploads/asset/source/620b38028f37ad4c544bf6b97e1f85ba/original.jpg",
+  description: "It reminds me of the day I stood in a field in the early morning.",
+  is_readable: false,
+  updated_at: "2014-07-07T16:03:44.563Z",
+  created_at: "2014-07-07T16:03:41.234Z",
+  year: null
+)
+Asset.create(
+  title: "The Queen",
+  url: "https://edinburgh-stories-staging.s3.amazonaws.com/uploads/asset/source/620b38028f37ad4c544bf6b97e1f8c9d/original.jpg",
+  is_readable: false,
+  updated_at: "2014-07-07T15:45:10.871Z",
+  created_at: "2014-07-07T15:45:04.910Z",
+  year: 2014
+)
+Asset.create(
+  title: "Central Library",
+  url: "https://edinburgh-stories-staging.s3.amazonaws.com/uploads/asset/source/620b38028f37ad4c544bf6b97e1f9703/original.jpg",
+  is_readable: false,
+  updated_at: "2014-07-07T15:40:01.943Z",
+  created_at: "2014-07-07T15:39:55.706Z",
+  year: 2014
+)
+Asset.create(
+  title: "Castle Rear",
+  file_type: "image",
+  url: "https://edinburgh-stories-staging.s3.amazonaws.com/uploads/asset/source/32fb7731-1a71-4072-84e7-3576f28470c0/image.jpg",
+  description: "Took this picture during a fire drill at Argyle House.",
+  year: "2014",
+  month: "",
+  day: "",
+  is_readable: false,
+  updated_at: "2014-07-18T23:15:03.908Z",
+  created_at: "2014-07-18T23:15:03.908Z",
+)
+Asset.create(
+  title: "Arthur's Seat",
+  file_type: "image",
+  url: "https://edinburgh-stories-staging.s3.amazonaws.com/uploads/asset/source/e32c68c2-6dfd-4dd7-86b7-968d0baf7370/meadows.jpg",
+  description: "",
+  year: "2010",
+  month: "",
+  day: "",
+  is_readable: false,
+  updated_at: "2014-07-18T23:12:13.079Z",
+  created_at: "2014-07-18T23:12:13.079Z",
+)
+Asset.create(
+  title: "Tattoo",
+  file_type: "image",
+  url: "https://edinburgh-stories-staging.s3.amazonaws.com/uploads/asset/source/724a0e50-8fe9-4e98-ae0e-ab78a5edac1e/tattoo.jpg",
+  description: "The military tattoo takes place every year at Edinburgh Castle.",
+  year: "2013",
+  month: "7",
+  day: "",
+  is_readable: false,
+  updated_at: "2014-07-18T23:11:01.095Z",
+  created_at: "2014-07-18T23:11:01.095Z",
+)
+Asset.create(
+  title: "Porty beach portrait",
+  url: "https://edinburgh-stories-staging.s3.amazonaws.com/uploads/asset/source/6540dddb1df0bd632afaee14b61f41a4/original.jpg",
+  is_readable: false,
+  updated_at: "2014-07-07T13:19:43.335Z",
+  created_at: "2014-07-07T13:19:39.201Z",
+  year: 2014
+)
+Asset.create(
+  title: "Porty beach now",
+  url: "https://edinburgh-stories-staging.s3.amazonaws.com/uploads/asset/source/6540dddb1df0bd632afaee14b61f44b1/original.jpg",
+  is_readable: false,
+  updated_at: "2014-07-07T13:19:03.518Z",
+  created_at: "2014-07-07T13:18:59.712Z",
+  year: 2014
+)
+Asset.create(
+  title: "Porty pier",
+  url: "https://edinburgh-stories-staging.s3.amazonaws.com/uploads/asset/source/6540dddb1df0bd632afaee14b61f4f6f/original.jpg",
+  is_readable: false,
+  updated_at: "2014-07-07T13:17:46.783Z",
+  created_at: "2014-07-07T13:17:43.584Z",
+  year: 1922
+)
+Asset.create(
+  title: "Porty beach",
+  url: "https://edinburgh-stories-staging.s3.amazonaws.com/uploads/asset/source/6540dddb1df0bd632afaee14b61f5c88/original.jpg",
+  is_readable: false,
+  updated_at: "2014-07-07T13:17:31.430Z",
+  created_at: "2014-07-07T13:17:28.138Z",
+  year: 1919
+)
+Asset.create(
+  title: "King's Road",
+  url: "https://edinburgh-stories-staging.s3.amazonaws.com/uploads/asset/source/6540dddb1df0bd632afaee14b61f5df1/original.jpg",
+  is_readable: false,
+  updated_at: "2014-07-07T13:17:11.764Z",
+  created_at: "2014-07-07T13:17:06.969Z",
+  year: 1918
+)
